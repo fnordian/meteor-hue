fe4de1f9937fdc7f794cdf806d76fa59fadc497a
Marcus Hunger <hunger@sipgate.de>
2015-08-20 15:43:33 +0200

    added versionsFrom to package.js

--- package.js
+++ package.js
@@ -3,9 +3,10 @@ Package.describe({
   version: '0.0.1',
   name: 'fnordian:hue',
   git: 'https://github.com/fnordian/meteor-hue'
-})
+});
 
 Package.on_use(function(api) {
+  api.versionsFrom('1.1.0.2');
   api.use("http", "server");
   api.add_files("hue.js", "server");
   api.export("Hue", "server");
